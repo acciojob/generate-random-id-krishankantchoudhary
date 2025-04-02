5a09d216b29b6c6411132fb98e875f7e3c55cb0e
krishan kant choudhary <83417448+krishankantchoudhary@users.noreply.github.com>
2025-04-02 10:20:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function makeid(l) {
   // write your code here
+	var res="";
+	var charlist="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	for(var i=0; i<l; i++){
+		res=res+charlist.charAt(
+			Math.floor(Math.random()*charlist.length)
+		)
+		)
+			
+	}
+	return res;
+	
 }
 
 // Do not change the code below.
